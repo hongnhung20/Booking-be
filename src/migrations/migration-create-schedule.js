5652a0d17c481d597057575abbae1b566523f93c
hongnhung20 <hongnhungtdm0905@gmail.com>
2023-07-24 14:21:15 +0700

    done

---
+++ src/migrations/migration-create-schedule.js
@@ -0,0 +1,44 @@
+// curentNumber: DataTypes.INTEGER,
+//       maxNumber: DataTypes.INTEGER,
+//       date: DataTypes.DATE,
+//       timeType: DataTypes.STRING,
+//       dotorId: DataTypes.INTEGER,
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Schedules", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      curentNumber: {
+        type: Sequelize.INTEGER,
+      },
+      maxNumber: {
+        type: Sequelize.INTEGER,
+      },
+      date: {
+        type: Sequelize.DATE,
+      },
+      timeType: {
+        type: Sequelize.STRING,
+      },
+      dotorId: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Schedules");
+  },
+};
